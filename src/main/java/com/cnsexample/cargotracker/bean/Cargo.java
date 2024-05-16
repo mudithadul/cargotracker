253d62221b2c13203f1d24a57636ec50cd4b4228
Muditha Sumanathunga <muditha.sumanathunga@madmobile.com>
2024-05-16 14:02:20 +0100

    Feat: add Class JPA data

--- src/main/java/com/cnsexample/cargotracker/bean/Cargo.java
+++ src/main/java/com/cnsexample/cargotracker/bean/Cargo.java
@@ -2,6 +2,19 @@ package com.cnsexample.cargotracker.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Null;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,9 +25,30 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Cargo {
+@Entity
+@Table(name = "CARGO")
+public class Cargo implements Serializable {
+
+    @Null(message = "ID is populated internally")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME", nullable = false)
     private String cargoNumber;
+
+    @Column(name = "WEIGHT", nullable = false, precision = 7, scale = 2)
     private BigDecimal weight;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "STATUS", nullable = false)
     private CargoStatus status;
+
+    @Column(name = "CONSIGNMENT_NUMBER", nullable = false, unique = true)
     private String consignmentNumber;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="FLIGHT_ID", nullable = false)
+    private Flight flight;
 }
